16cd27871bb36d441392206752270bc2cf0fea7a
tyhus14 <huston.tyler3@gmail.com>
2014-07-22 16:18:01 -0400

    updated nav hovers

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,6 +11,28 @@ $(function() {
 
     });
 
+    $(".scrollto").bind("click", function(e) {
+
+        e.preventDefault();
+        var location = $(this).attr("href")
+
+        $('html, body').animate({
+            scrollTop: $(location).offset().top - 80
+        }, 1500);
+
+    });
+
+    $('.navbar-home').hover(function() {
+        $('.home').toggleClass('active');
+    });
+
+    $('.navbar-experience').hover(function() {
+        $(".experience-js").toggleClass('active');
+    });
+
+    $('.navbar-contact').hover(function() {
+        $(".contact-js").toggleClass('active');
+    });
 
 
 });
